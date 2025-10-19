b3cea766449e16e50edf0ce5c242de8406625f85
Priyanshu Verma <vpriyanshu864@gmail.com>
2025-10-19 20:25:15 +0530

    refact- Updated Navbar component to remove the background image and adjust layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ function NavBar() {
     <div className="px-5 py-8 bg-blue-500 opacity-90 relative overflow-hidden">
        <div
           className='absolute right-[-15%] top-0 h-full lg:w-[60%] md:w-[80%]  bg-no-repeat bg-contain'
-          style={{ backgroundImage: "url('/bg-image.png')" }}
+         
         ></div>
         <img src="./GrowMeOrganicLogo.webp" alt="" />
     </div>
